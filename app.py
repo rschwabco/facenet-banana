74f5206f4416d624cf152038d94c4b5bc15abca7
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-02-08 16:02:19 -0800

    init

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
-from transformers import pipeline
+from typing import Dict, List, Any
 import torch
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import base64
 
-# Init is ran on server startup
-# Load your model to GPU as a global variable here using the variable name "model"
+from facenet_pytorch import MTCNN, InceptionResnetV1
+
 def init():
-    global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    global device
+    global mtcnn
+    global resnet
 
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
-    global model
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    mtcnn = MTCNN(device=device)
+    resnet = InceptionResnetV1(pretrained='vggface2', device=device).eval()
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
+def inference(data: Dict[str, Any]) -> List[Dict[str, Any]]:
+    global device
+    global mtcnn
+    global resnet
 
-    # Return the results as a dictionary
-    return result
+    imageData = data.get("inputs").get("image")
+    image = Image.open(BytesIO(base64.b64decode(imageData)))
+    face_batch = mtcnn([image])
+    face_batch = [i for i in face_batch if i is not None]
+    if face_batch:
+        aligned = torch.stack(face_batch)
+        if device.type == "cuda":
+            aligned = aligned.to(device)
+
+        embeddings = resnet(aligned).detach().cpu()
+        return embeddings.tolist()
+    else: return None
